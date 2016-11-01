17cf85cfb08df0d68b9f69ed1e30159941556fee
thiagotmb <tmb0710@gmail.com>
2016-11-01 07:51:34 -0200

    Adding responsive layout of action detail box

--- app/components/home-page-component.js
+++ app/components/home-page-component.js
@@ -4,10 +4,22 @@ export default Ember.Component.extend({
 
   actionBarComponentsActive: [false,false,false,false],
 
+  haveActionBarComponentActive:  Ember.computed('actionBarComponentsActive', function() {
+    let haveActiveComponent = this.actionBarComponentsActive.filter(function(actionBarItemIsActive){
+      return (actionBarItemIsActive == true);
+    }).length > 0
+    return haveActiveComponent
+  }),
+
   actions: {
       selectItem(item) {
-        this.set('actionBarComponentsActive',[false,false,false,false])
-        this.set('actionBarComponentsActive.'+item, !this.actionBarComponentsActive[item])
+        if (this.actionBarComponentsActive[item] == true) {
+          this.set('actionBarComponentsActive',[false,false,false,false])
+        } else {
+          this.set('actionBarComponentsActive',[false,false,false,false])
+          this.set('actionBarComponentsActive.'+item, true)
+        }
+
       }
     }
 });
