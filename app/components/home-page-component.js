1dd22ffba7fae3df17b1b3236115b8541f2ba6be
thiagotmb <tmb0710@gmail.com>
2016-11-01 07:57:23 -0200

    Create variables for html texts

--- app/components/home-page-component.js
+++ app/components/home-page-component.js
@@ -4,6 +4,15 @@ export default Ember.Component.extend({
 
   actionBarComponentsActive: [false,false,false,false],
 
+  contentObject: {
+    mainPhraseTitle: "SONHE. FAÇA. COMPARTILHE.",
+    mainPhraseDescription: "Somos uma Co-lab que oferece comunidade, espaço e conhecimento que atendem às necessidades de uma nova geração de trabalhadores: mais criativos, colaborativos e inquietos.",
+    actionLeftTitle: "COMUNIDADE",
+    actionCenter0Title: "ESPAÇO DE TRABALHO",
+    actionCenter1Title: "CONHECIMENTO",
+    actionRightTitle: "CONTATO"
+  },
+
   haveActionBarComponentActive:  Ember.computed('actionBarComponentsActive', function() {
     let haveActiveComponent = this.actionBarComponentsActive.filter(function(actionBarItemIsActive){
       return (actionBarItemIsActive == true);
