7598a3612ba581bdd116673a6b5b22d63765e7a9
luisresende <luisfrvilela@gmail.com>
2017-01-19 17:34:02 -0200

    adding content in js community benefits

--- app/components/community-benefits-container.js
+++ app/components/community-benefits-container.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    title:"MOTIVOS DE SOBRA PARA SE CONECTAR",
+    firstButton:"FAÇA PARTE",
+    secondButton:"CONHEÇA NOSSOS PLANOS",
+
+    contentObjectLeftUp: {
+        title: "MEMBERSHIP",
+        description: "Acesso a nossa plataforma digital com mais de 400 empreendedores, benefícios exclusivos para membros, programas de conteúdo e eventos para networking. Também inclui 1 crédito de “meeting room”ou 1 crédito de “Open Desk” por mês e a possibilidade de comprar mais quando necessário.",
+        image:"/assets/images/icon_network.png"
+    },
+
+    contentObjectRightUp: {
+        title: "MEMBERSHIP",
+        description: "Acesso a nossa plataforma digital com mais de 400 empreendedores, benefícios exclusivos para membros, programas de conteúdo e eventos para networking. Também inclui 1 crédito de “meeting room”ou 1 crédito de “Open Desk” por mês e a possibilidade de comprar mais quando necessário.",
+        image:"/assets/images/icon_space.png"
+    },
+
+    contentObjectLeftDown: {
+        title: "MEMBERSHIP",
+        description: "Acesso a nossa plataforma digital com mais de 400 empreendedores, benefícios exclusivos para membros, programas de conteúdo e eventos para networking. Também inclui 1 crédito de “meeting room”ou 1 crédito de “Open Desk” por mês e a possibilidade de comprar mais quando necessário.",
+        image:"/assets/images/icon_events.png"
+    },
+
+    contentObjectRightDown: {
+        title: "MEMBERSHIP",
+        description: "Acesso a nossa plataforma digital com mais de 400 empreendedores, benefícios exclusivos para membros, programas de conteúdo e eventos para networking. Também inclui 1 crédito de “meeting room”ou 1 crédito de “Open Desk” por mês e a possibilidade de comprar mais quando necessário.",
+        image:"/assets/images/icon_discounts.png"
+    }
+
 });
