625f63addf1a82a291cf55178d3a5af066ef7c71
luisresende <luisfrvilela@gmail.com>
2017-01-19 18:00:08 -0200

    adding real content in community relationship

--- app/components/community-relationship-container.js
+++ app/components/community-relationship-container.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    contentMainObject:{
+        title:"RELACIONAMENTO É A NOSSA VOCAÇÃO",
+        description:"Ao explorar todos os benefícios da nossa rede, você pode encontrar parceiros e clientes para o seu negócio, compartilhar e adquirir novas experiências e ficar por dentro dos melhores eventos de empreendedorismo da região. Se identificou e quer fazer parte? Veja abaixo suas opções:"
+    }
 });
