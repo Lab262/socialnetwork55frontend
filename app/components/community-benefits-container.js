38d11292ea98d3c8154f1c899b8f05a3ac1efc85
luisresende <luisfrvilela@gmail.com>
2017-01-19 17:47:49 -0200

    adding real content in community benefits

--- app/components/community-benefits-container.js
+++ app/components/community-benefits-container.js
@@ -1,31 +1,35 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    title:"MOTIVOS DE SOBRA PARA SE CONECTAR",
-    firstButton:"FAÇA PARTE",
-    secondButton:"CONHEÇA NOSSOS PLANOS",
-
+    contentMainObject:{
+        title:"MOTIVOS DE SOBRA PARA SE CONECTAR",
+        firstButton:"FAÇA PARTE",
+        secondButton:"CONHEÇA NOSSOS PLANOS"
+    },
+    
     contentObjectLeftUp: {
-        title: "MEMBERSHIP",
-        description: "Acesso a nossa plataforma digital com mais de 400 empreendedores, benefícios exclusivos para membros, programas de conteúdo e eventos para networking. Também inclui 1 crédito de “meeting room”ou 1 crédito de “Open Desk” por mês e a possibilidade de comprar mais quando necessário.",
+        title: "NETWORKING",
+        description: "Conexão on e offline com uma base que já começa com mais de 400 empreendedores, mentores, colaboradores e investidores para compartilhar ideias, experiências e oportunidades de trabalho.",
         image:"/assets/images/icon_network.png"
     },
 
     contentObjectRightUp: {
-        title: "MEMBERSHIP",
-        description: "Acesso a nossa plataforma digital com mais de 400 empreendedores, benefícios exclusivos para membros, programas de conteúdo e eventos para networking. Também inclui 1 crédito de “meeting room”ou 1 crédito de “Open Desk” por mês e a possibilidade de comprar mais quando necessário.",
+        title: "ACESSO AO ESPAÇO",
+        //Falta colocar links onde ta (link)
+        description: "Acesso flexível ao espaço: como membro, você terá 01 hora em meeting room (link) ou open desk (link) por mês e ficará por dentro de tudo que rola nos espaços de trabalho com exclusividade.",
         image:"/assets/images/icon_space.png"
     },
 
     contentObjectLeftDown: {
-        title: "MEMBERSHIP",
-        description: "Acesso a nossa plataforma digital com mais de 400 empreendedores, benefícios exclusivos para membros, programas de conteúdo e eventos para networking. Também inclui 1 crédito de “meeting room”ou 1 crédito de “Open Desk” por mês e a possibilidade de comprar mais quando necessário.",
+        title: "EVENTOS EXCLUSIVOS",
+        description: "A +55Lab.co promove uma série de eventos sociais, palestras, programas de captação de workshops e cursos nos quais você pode adquirir conhecimento e compartilhar ideias sobre o universo empreendedor.",
         image:"/assets/images/icon_events.png"
     },
 
     contentObjectRightDown: {
-        title: "MEMBERSHIP",
-        description: "Acesso a nossa plataforma digital com mais de 400 empreendedores, benefícios exclusivos para membros, programas de conteúdo e eventos para networking. Também inclui 1 crédito de “meeting room”ou 1 crédito de “Open Desk” por mês e a possibilidade de comprar mais quando necessário.",
+        title: "DESCONTOS ESPECIAIS",
+        //Falta colocar links onde ta (link)
+        description: "Quem é membro tem a sua disposição uma rede de descontos em serviços que ajudam o empreendedor a cuidar melhor de si, da sua empresa e de seus colaboradores. Serviços jurídicos e de comunicação a um preço mais em conta e descontos em planos de saúde, seguros e ferramentas são alguns benefícios que estão disponíveis. Acompanhe no blog (link) as novidades.",
         image:"/assets/images/icon_discounts.png"
     }
 
