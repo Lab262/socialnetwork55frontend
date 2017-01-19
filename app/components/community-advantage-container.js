59ba73403885e57634c6f96d56b25ab05d028463
Huallyd <huallyd@gmail.com>
2017-01-19 13:36:19 -0200

    js object in community advantage

--- app/components/community-advantage-container.js
+++ app/components/community-advantage-container.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+   contentObject: {
+    title: "MEMBERSHIP",
+    descriptionFirst: "Acesso a nossa plataforma digital com mais de 400 empreendedores, benefícios exclusivos para membros, programas de conteúdo e eventos para networking. Também inclui 1 crédito de “meeting room”ou 1 crédito de “Open Desk” por mês e a possibilidade de comprar mais quando necessário.",
+    descriptionSecond: "Um plano perfeito (mês a mês) para quem quer começar a se conectar com a comunidade empreendedora, mas precisa do espaço de trabalho poucos dias no mês."
+  }
 });
