e431ca0ca6318629c2c1ff7b535ababe46f65222
luisresende <luisfrvilela@gmail.com>
2017-01-20 13:42:53 -0200

    adding real content in events in knowledge

--- app/components/knowledge-events-container.js
+++ app/components/knowledge-events-container.js
@@ -1,4 +1,80 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    event1Object:{
+        title:"Peteleco",
+        local:"55Lab.Store",
+        description:"O Peteleco é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ.",
+        descriptionOrganizer:"Acelere.me",
+        descriptionSupporter:"55Lab.Store",
+        descriptionDate:"16 de fevereiro e 16 de março",
+        descriptionAmount:"indefinida",
+        descriptionMoreInformation:"Empreender é um estilo de vida, um estado de espírito. Já em sua terceira edição, o evento impactou mais de 400 empreendedores ao longo de 2016."
+    },
+
+    event2Object:{
+        title:"A Nova Lei do Investidor Anjo",
+        local:"55Lab.Work",
+        description:"MODIFICAR é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ.",
+        descriptionOrganizer:"Acelere.me",
+        descriptionSupporter:"55Lab.Work",
+        descriptionDate:"09 de março",
+        descriptionAmount:"15 pessoas",  
+        descriptionMoreInformation:"MODIFICAR é um estilo de vida, um estado de espírito. Já em sua terceira edição, o evento impactou mais de 400 empreendedores ao longo de 2016."
+    },
+
+    event3Object:{
+        title:"Noções Jurídicas para usuários",
+        local:"55Lab.Store",
+        description:"MODIFICAR é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ.",
+        descriptionOrganizer:"4E20.CLUB",
+        descriptionSupporter:"Acelere.me e 55Lab.Store",
+        descriptionDate:"14 de março",
+        descriptionAmount:"10 pessoas",  
+        descriptionMoreInformation:"MODIFICAR é um estilo de vida, um estado de espírito. Já em sua terceira edição, o evento impactou mais de 400 empreendedores ao longo de 2016."
+    },
+
+    event4Object:{
+        title:"Acelereção de negócios de lifestyle",
+        local:"55Lab.Store",
+        description:"MODIFICAR é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ.",
+        descriptionOrganizer:"Acelere.me",
+        descriptionSupporter:"55Lab.Store",
+        descriptionDate:"28 de março",
+        descriptionAmount:"10 pessoas",  
+        descriptionMoreInformation:"MODIFICAR é um estilo de vida, um estado de espírito. Já em sua terceira edição, o evento impactou mais de 400 empreendedores ao longo de 2016."
+    },
+
+    event5Object:{
+        title:"Tapa na Cara",
+        local:"55Lab.Work",
+        description:"O Tapa na Cara é um evento que tem 16 horas seguidas de capacitação por meio de experiência empreendedora com a metodologia de implementação de negócios do Acelere.me.",
+        descriptionOrganizer:"Acelere.me",
+        descriptionSupporter:"55Lab.Work",
+        descriptionDate:"04 e 06 de abril",
+        descriptionAmount:"15 pessoas",  
+        descriptionMoreInformation:"Este evento busca dar um “tapa na cara” dos participantes para que eles possam entender que, apesar de não ser fácil, empreender com planejamento, metas e objetivos claros é possível e muito menos arriscado."
+    },
+
+    event6Object:{
+        title:"Experience",
+        local:"MODIFICAR",
+        description:"MODIFICAR é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ.",
+        descriptionOrganizer:"MODIFICAR",
+        descriptionSupporter:"MODIFICAR",
+        descriptionDate:"MODIFICAR",
+        descriptionAmount:"MODIFICAR",  
+        descriptionMoreInformation:"No Experience você pode participar de um dia de trabalho para entrar em contato com  a metodologia de implementação de negócios do Acelere.me, descobrir a importância de definição de propósito para o seu projeto e aprender sobre desenvolvimento de modelo de negócios, metas e plano de ação."
+    },
+
+    event7Object:{
+        title:"Maratona Ignition",
+        local:"CTJ",
+        description:"Comprometido com o desenvolvimento econômico local e com o objetivo de estimular a criação de pequenas e médias empresas de vanguarda o Maratona Ignition é conhecido como o primeiro programa de pré-aceleração de negócios e empresas desenvolvido no Distrito Federal.",
+        descriptionOrganizer:"Acelere.me",
+        descriptionSupporter:"55Lab",
+        descriptionDate:"14,15 e 16 de julho",
+        descriptionAmount:"50 pessoas",  
+        descriptionMoreInformation:"Todo ano, 10 ideias e projetos de negócios são selecionados para participar de 48h intensas de capacitações com experiências empreendedoras e mentorias com profissionais do mercado. Para essa seleção são avaliados o modelo de negócios, a equipe, o potencial de inovação, e a viabilidade financeira do projeto. Na edição de 2016, participaram 10 projetos e, desses, 6 negócios já foram implementados e estão em fase de tração. A ideia melhor avaliada – Be.Cause -  ainda recebeu um aporte de R$14.000,00, 4 meses de mentorias e estrutura física de trabalho e está em fase de lançamento da segunda coleção."
+    }
 });
