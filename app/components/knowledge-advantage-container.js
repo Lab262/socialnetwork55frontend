021ab8b6558c269c32e98df13715e46494e8ffe9
luisresende <luisfrvilela@gmail.com>
2017-01-22 00:53:02 -0200

    adding real content in description knowledge

--- app/components/knowledge-advantage-container.js
+++ app/components/knowledge-advantage-container.js
@@ -4,6 +4,6 @@ export default Ember.Component.extend({
     contentObject:{
         title:"ACELERE.ME",
         firstDescription:"O Acelere.me é um ecossistema que estimula o produtivismo e a inclusão, que fomenta a vivência empreendedora. O seu time é formado por pessoas que acreditam que o empreendedorismo é o principal drive para o desenvolvimento sustentável e que a troca de experiências e capacitações são imprescindíveis para que novas ideias se estabeleçam como negócios sustentáveis.",
-        secondDescription:"Veja alguns eventos de capacitação e relacionamento que o Acelere.me produz para a comunidade empreendedora brasiliense:"
+        secondDescription:"Através disso o Acelere.me atua em três principais frentes: conhecimento, mentorias e comunidade."
     }
 });
