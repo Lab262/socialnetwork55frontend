2b36b3e144725fa77db3e41c54b8e7ad5579a4ec
luisresende <luisfrvilela@gmail.com>
2017-01-22 01:06:39 -0200

    content knowdlege opinion in js

--- app/components/knowledge-opnion-container.js
+++ app/components/knowledge-opnion-container.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    firstOpinionObject:{
+        name:"ALLAN SOUZA",
+        position:"CEO AGÊNCIA A PROPOSITO",
+        opinion:"“Na 55 lab pude conquistar meu espaço como empreendedor e estar no centro do mercado criativo em Brasília. Melhor que isso, não tem.”"
+    },
+
+    secondOpinionObject:{
+        name:"ALLAN SOUZA",
+        position:"CEO AGÊNCIA A PROPOSITO",
+        opinion:"“Na 55 lab pude conquistar meu espaço como empreendedor e estar no centro do mercado criativo em Brasília. Melhor que isso, não tem.”"
+    }
 });
