d62a5ca3bab0d63e8a0154570aa87682f920c403
thiagotmb <tmb0710@gmail.com>
2017-01-27 21:29:42 -0500

    Adding gtm

--- config/environment.js
+++ config/environment.js
@@ -19,6 +19,10 @@ module.exports = function(environment) {
     }
   };
 
+  ENV['ember-cli-gtm'] = {
+  appId: 'GTM-P62ZBH4'
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
