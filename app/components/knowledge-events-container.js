8f50bcd06233d2e5044411ff0b357d6e058bd85a
luisresende <luisfrvilela@gmail.com>
2017-01-31 02:27:15 -0200

    refactors in designer

--- app/components/knowledge-events-container.js
+++ app/components/knowledge-events-container.js
@@ -15,34 +15,34 @@ export default Ember.Component.extend({
     event2Object:{
         title:"A Nova Lei do Investidor Anjo",
         local:"55Lab.Work",
-        description:"MODIFICAR é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ.",
+        description:"Quer saber como ficaram as regras para receber ou fazer investimento anjo no Brasil?",
         descriptionOrganizer:"Acelere.me",
         descriptionSupporter:"55Lab.Work",
         descriptionDate:"09 de março",
         descriptionAmount:"15 pessoas",  
-        descriptionMoreInformation:"MODIFICAR é um estilo de vida, um estado de espírito. Já em sua terceira edição, o evento impactou mais de 400 empreendedores ao longo de 2016."
+        descriptionMoreInformation:"No dia 1º de janeiro de 2017 entrou em vigor a Lei complementar nº 155 que define estrutura de investimento-anjo e segurança jurídica para esta modalidade de aporte de capital. Atentos a essa nova realidade montamos um curso para orientar pessoas físicas e jurídicas que queiram fazer aportes de capital e/ou empreendedores que necessitam de investimento."
     },
 
     event3Object:{
         title:"Noções Jurídicas para usuários",
         local:"55Lab.Store",
-        description:"MODIFICAR é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ.",
+        description:"Está interessado em cultivar cannabis, mas tem medo de ser preso?",
         descriptionOrganizer:"4E20.CLUB",
         descriptionSupporter:"Acelere.me e 55Lab.Store",
         descriptionDate:"14 de março",
         descriptionAmount:"10 pessoas",  
-        descriptionMoreInformation:"MODIFICAR é um estilo de vida, um estado de espírito. Já em sua terceira edição, o evento impactou mais de 400 empreendedores ao longo de 2016."
+        descriptionMoreInformation:"Preparamos um workshop para mostrar a atual realidade jurídico/social no Brasil em relação a cannabis e ao usuário. O objetivo do workshop é orientar e educar os cidadãos que são usuários de cannabis a minimizarem seus riscos jurídicos ao iniciarem um cultivo ou clube, terem mais informação para lutar pelo seus direitos de usuário (recreativo ou medicinal) e terem um panorama geral das decisões do judiciário brasileiro em relação a auto cultivo, aquisição de sementes e clubes sociais canábicos."
     },
 
     event4Object:{
         title:"Acelereção de negócios de lifestyle",
         local:"55Lab.Store",
-        description:"MODIFICAR é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ.",
+        description:"Sabia que é possível montar um negócio relacionado a cannabis de forma legal no Brasil?",
         descriptionOrganizer:"Acelere.me",
         descriptionSupporter:"55Lab.Store",
         descriptionDate:"28 de março",
         descriptionAmount:"10 pessoas",  
-        descriptionMoreInformation:"MODIFICAR é um estilo de vida, um estado de espírito. Já em sua terceira edição, o evento impactou mais de 400 empreendedores ao longo de 2016."
+        descriptionMoreInformation:"Isso é o que chamamos de empreendedorismo de lifestyle. Somos pioneiros na disseminação deste conceito e no apoio a estes negócios em nosso país, contribuindo para torná-los mais liberado, mais regulado, mais conhecido e reafirmá-lo como tendência de negócio emergente na sociedade brasileira. Diante disso, preparamos um workshop para orientar e capacitar empreendedores que queiram fazer ou tenham negócios nesse mercado de lifestyle."
     },
 
     event5Object:{
