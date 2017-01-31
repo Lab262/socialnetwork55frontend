b0591ec3f157db9cfc9bdcf5ab104d58b29e3c33
luisresende <luisfrvilela@gmail.com>
2017-01-31 17:15:54 -0200

    implementing subtitles in pages and content in js

--- app/components/knowledge-fixed-background.js
+++ app/components/knowledge-fixed-background.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    contentObject:{
+        title:"Conhecimento",
+        subtitle:"Acreditamos que por meio da troca de diferentes experiências empreendedoras podemos fazer com que, cada vez mais, nossa comunidade expanda, conheça novos horizontes e chegue mais longe."
+    }
 });
--- app/components/work-space-fixed-background.js
+++ app/components/work-space-fixed-background.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    contentObject:{
+        title:"Espaços de trabalho",
+        subtitle:"Nossos espaços de trabalho foram planejados com foco em produtividade, estilo, flexibilidade e na sua vontade de compartilhar experiências. Sabemos que nem todos os negócios são iguais, por isso temos três opções disponíveis."
+    }
 });
