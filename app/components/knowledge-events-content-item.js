f5d7e93e36837221963f5f23eb4c997c664f55ad
luisresende <luisfrvilela@gmail.com>
2017-02-05 23:28:07 -0200

    change design constants

--- app/components/knowledge-events-content-item.js
+++ app/components/knowledge-events-content-item.js
@@ -3,21 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     mainObject:{
         titleButton:"PARTICIPAR",
-        titleButtonMembership:"SEJA MEMBERSHIP",
+        titleButtonMembership:"SEJA MEMBRO",
         nameOrganizer:"ORGANIZADOR",
         nameSupporter:"APOIADOR",
         nameDate:"DATA",
         nameAmount:"LOTAÇÃO",
         nameMoreInformation:"INFORMAÇÕES ADICIONAIS",
-    },
-    eventObject:{
-        title:"laskdlaskdlaskd",
-        local:"laskdlaskdlaskd",
-        description:"laskdlaskdlaskd",
-        descriptionOrganizer:"laskdlaskdlaskd",
-        descriptionSupporter:"laskdlaskdlaskd",
-        descriptionDate:"laskdlaskdlaskd",
-        descriptionAmount:"laskdlaskdlaskd",
-        descriptionMoreInformation:"laskdlaskdlaskd"
     }
 });
