c66e60858c20931bb76a86dfd89c386f1e5dfbb7
luisresende <luisfrvilela@gmail.com>
2017-02-09 01:40:56 -0200

    refactor in knowledge page

--- app/components/knowledge-advantage-container.js
+++ app/components/knowledge-advantage-container.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     contentObject:{
-        title:"ACELERE.ME",
+        image:"/assets/images/logo-acelere-me-knowledge.png",
         firstDescription:"O Acelere.me é um ecossistema que estimula o produtivismo e a inclusão, que fomenta a vivência empreendedora. O seu time é formado por pessoas que acreditam que o empreendedorismo é o principal drive para o desenvolvimento sustentável e que a troca de experiências e capacitações são imprescindíveis para que novas ideias se estabeleçam como negócios sustentáveis.",
         secondDescription:"Através disso o Acelere.me atua em três principais frentes: conhecimento, mentorias e comunidade."
     }
--- app/components/knowledge-events-content-item.js
+++ app/components/knowledge-events-content-item.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     mainObject:{
         titleButton:"PARTICIPAR",
+        titleSecondButton:"SAIBA MAIS",
         titleButtonMembership:"SEJA MEMBRO",
         nameOrganizer:"ORGANIZADOR",
         nameSupporter:"APOIADOR",
