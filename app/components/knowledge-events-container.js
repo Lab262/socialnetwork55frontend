c8a6723391ffb2c3879e56c064af7de1da9fd400
thiagotmb <tmb0710@gmail.com>
2017-02-09 01:43:18 -0500

    Adding eventick integration

--- app/components/knowledge-events-container.js
+++ app/components/knowledge-events-container.js
@@ -1,87 +1,125 @@
 import Ember from 'ember';
+import Eventick from "npm:node-eventick"
 
 export default Ember.Component.extend({
-    event1Object:{
-        title:"Peteleco",
-        local:"55Lab.Store",
-        image:"/assets/images/image-curse-peteleco.png",
-        description:"Peteleco é o evento do Acelere.me que vai te dar aquele toque esperto pra você perceber que empreender é muito mais do que ter uma empresa ou um CNPJ.",
-        descriptionOrganizer:"Acelere.me",
-        descriptionSupporter:"55Lab.Store",
-        descriptionDate:"16 de fevereiro e 16 de março",
-        descriptionAmount:"indefinida",
-        descriptionMoreInformation:"O Peteleco é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ. Empreender é um estilo de vida, um estado de espírito. Já em sua terceira edição, o evento impactou mais de 400 empreendedores ao longo de 2016."
+
+    events: [
+        {
+            title: "Peteleco",
+            local: "55Lab.Store",
+            image: "/assets/images/image-curse-peteleco.png",
+            description: "Peteleco é o evento do Acelere.me que vai te dar aquele toque esperto pra você perceber que empreender é muito mais do que ter uma empresa ou um CNPJ.",
+            descriptionOrganizer: "Acelere.me",
+            descriptionSupporter: "55Lab.Store",
+            descriptionDate: "16 de fevereiro e 16 de março",
+            descriptionAmount: "indefinida",
+            descriptionMoreInformation: "O Peteleco é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ. Empreender é um estilo de vida, um estado de espírito. Já em sua terceira edição, o evento impactou mais de 400 empreendedores ao longo de 2016."
+        },
+
+        {
+            title: "A Nova Lei do Investidor Anjo",
+            local: "55Lab.Work",
+            image: "/assets/images/background-image.png",
+            description: "Entenda como ficaram as regras para receber ou fazer investimento anjo no Brasil depois que a Lei complementar nº 155 entrou em vigor no país.",
+            descriptionOrganizer: "Acelere.me",
+            descriptionSupporter: "55Lab.Work",
+            descriptionDate: "09 de março",
+            descriptionAmount: "15 pessoas",
+            descriptionMoreInformation: "No dia 1º de janeiro de 2017 entrou em vigor a Lei complementar nº 155 que define estrutura de investimento-anjo e segurança jurídica para esta modalidade de aporte de capital. Atentos a essa nova realidade montamos um curso para orientar pessoas físicas e jurídicas que queiram fazer aportes de capital e/ou empreendedores que necessitam de investimento."
+        },
+        {
+            title: "Noções Jurídicas para usuários",
+            local: "55Lab.Store",
+            image: "/assets/images/background-image.png",
+            description: "Saiba + sobre a atual realidade jurídico/social da cannabis e do usuário no Brasil.",
+            descriptionOrganizer: "4E20.CLUB",
+            descriptionSupporter: "Acelere.me e 55Lab.Store",
+            descriptionDate: "14 de março",
+            descriptionAmount: "10 pessoas",
+            descriptionMoreInformation: "Preparamos um workshop para mostrar a atual realidade jurídico/social no Brasil em relação a cannabis e ao usuário. O objetivo do workshop é orientar e educar os cidadãos que são usuários de cannabis a minimizarem seus riscos jurídicos ao iniciarem um cultivo ou clube, terem mais informação para lutar pelo seus direitos de usuário (recreativo ou medicinal) e terem um panorama geral das decisões do judiciário brasileiro em relação a auto cultivo, aquisição de sementes e clubes sociais canábicos."
+        },
+        {
+            title: "Acelereção de negócios de lifestyle",
+            local: "55Lab.Store",
+            image: "/assets/images/background-image.png",
+            description: "Sabia que é possível montar um negócio relacionado a cannabis de forma legal no Brasil?",
+            descriptionOrganizer: "Acelere.me",
+            descriptionSupporter: "55Lab.Store",
+            descriptionDate: "28 de março",
+            descriptionAmount: "10 pessoas",
+            descriptionMoreInformation: "Isso é o que chamamos de empreendedorismo de lifestyle. Somos pioneiros na disseminação deste conceito e no apoio a estes negócios em nosso país, contribuindo para torná-los mais liberado, mais regulado, mais conhecido e reafirmá-lo como tendência de negócio emergente na sociedade brasileira. Diante disso, preparamos um workshop para orientar e capacitar empreendedores que queiram fazer ou tenham negócios nesse mercado de lifestyle."
+        },
+        {
+            title: "Tapa na Cara",
+            local: "55Lab.Work",
+            image: "/assets/images/image-curse-tapa-cara.png",
+            description: "Um choque de realidade para você entender que empreender com planejamento é possível e muito menos arriscado!",
+            descriptionOrganizer: "Acelere.me",
+            descriptionSupporter: "55Lab.Work",
+            descriptionDate: "04 e 06 de abril",
+            descriptionAmount: "15 pessoas",
+            descriptionMoreInformation: "O Tapa na Cara é um evento que tem 16 horas seguidas de capacitação por meio de experiência empreendedora com a metodologia de implementação de negócios do Acelere.me. Este evento busca dar um “tapa na cara” dos participantes para que eles possam entender que, apesar de não ser fácil, empreender com planejamento, metas e objetivos claros é possível e muito menos arriscado."
+        },
+        {
+            title: "Experience",
+            local: "MODIFICAR",
+            image: "/assets/images/background-image.png",
+            description: "MODIFICAR é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ.",
+            descriptionOrganizer: "MODIFICAR",
+            descriptionSupporter: "MODIFICAR",
+            descriptionDate: "MODIFICAR",
+            descriptionAmount: "MODIFICAR",
+            descriptionMoreInformation: "No Experience você pode participar de um dia de trabalho para entrar em contato com  a metodologia de implementação de negócios do Acelere.me, descobrir a importância de definição de propósito para o seu projeto e aprender sobre desenvolvimento de modelo de negócios, metas e plano de ação."
+        },
+        {
+            title: "Maratona Ignition",
+            local: "CTJ",
+            image: "/assets/images/background-image.png",
+            description: "Evento do Acelere.me que busca pré-acelerar negócios e empresas por meio de intensas capacitações e mentorias com profissionais do mercado.",
+            descriptionOrganizer: "Acelere.me",
+            descriptionSupporter: "55Lab",
+            descriptionDate: "14,15 e 16 de julho",
+            descriptionAmount: "50 pessoas",
+            descriptionMoreInformation: "O Maratona Ignition é conhecido como o primeiro programa de pré-aceleração de negócios e empresas desenvolvido no Distrito Federal. Na última edição, 10 ideias e projetos de negócios foram selecionados para participar de 48h intensas de capacitações com experiências empreendedoras e mentorias com profissionais do mercado."
+        }
+
+    ],
+
+    didInsertElement() {
+        this._super(...arguments);
+
+        this.loadEventickData();
     },
 
-    event2Object:{
-        title:"A Nova Lei do Investidor Anjo",
-        local:"55Lab.Work",
-        image:"/assets/images/background-image.png",
-        description:"Entenda como ficaram as regras para receber ou fazer investimento anjo no Brasil depois que a Lei complementar nº 155 entrou em vigor no país.",
-        descriptionOrganizer:"Acelere.me",
-        descriptionSupporter:"55Lab.Work",
-        descriptionDate:"09 de março",
-        descriptionAmount:"15 pessoas",  
-        descriptionMoreInformation:"No dia 1º de janeiro de 2017 entrou em vigor a Lei complementar nº 155 que define estrutura de investimento-anjo e segurança jurídica para esta modalidade de aporte de capital. Atentos a essa nova realidade montamos um curso para orientar pessoas físicas e jurídicas que queiram fazer aportes de capital e/ou empreendedores que necessitam de investimento."
+    loadEventickData: function() {
+        var eventick = new Eventick('jguimaraes@4legal.com.br', 'juliana30');
+        var allEvents = eventick.events.list()
+        // var futureEvents = allEvents.filter(this.checkIfIsFutureEvent)
+
+        this.set('events', [])
+        var parsedEvents = []
+        for (var i = 0; i < allEvents.length; i++) {
+            var currentEventItem = allEvents[i]
+            currentEventItem = eventick.events.get(currentEventItem.id)
+            var parsedEventObject = {
+                title: currentEventItem.title,
+                image: currentEventItem.thumbnail_url,
+                description: "LOCAL: "+ currentEventItem.venue,
+                url: "https://www.eventick.com.br/" + currentEventItem.slug
+            }
+            parsedEvents.push(parsedEventObject)
+        }
+        this.set('events', parsedEvents)
     },
 
-    event3Object:{
-        title:"Noções Jurídicas para usuários",
-        local:"55Lab.Store",
-        image:"/assets/images/background-image.png",
-        description:"Saiba + sobre a atual realidade jurídico/social da cannabis e do usuário no Brasil.",
-        descriptionOrganizer:"4E20.CLUB",
-        descriptionSupporter:"Acelere.me e 55Lab.Store",
-        descriptionDate:"14 de março",
-        descriptionAmount:"10 pessoas",  
-        descriptionMoreInformation:"Preparamos um workshop para mostrar a atual realidade jurídico/social no Brasil em relação a cannabis e ao usuário. O objetivo do workshop é orientar e educar os cidadãos que são usuários de cannabis a minimizarem seus riscos jurídicos ao iniciarem um cultivo ou clube, terem mais informação para lutar pelo seus direitos de usuário (recreativo ou medicinal) e terem um panorama geral das decisões do judiciário brasileiro em relação a auto cultivo, aquisição de sementes e clubes sociais canábicos."
-    },
+    checkIfIsFutureEvent: function (eventObject) {
+        var date = new Date()
+        var dateISO = date.toISOString()
+        var convertedTodayDate = dateISO.substring(0, 10)
+        var currentItem = eventObject
+        var reducedItemStartDay = currentItem.start_at.substring(0, 10)
+        return reducedItemStartDay > convertedTodayDate
 
-    event4Object:{
-        title:"Acelereção de negócios de lifestyle",
-        local:"55Lab.Store",
-        image:"/assets/images/background-image.png",
-        description:"Sabia que é possível montar um negócio relacionado a cannabis de forma legal no Brasil?",
-        descriptionOrganizer:"Acelere.me",
-        descriptionSupporter:"55Lab.Store",
-        descriptionDate:"28 de março",
-        descriptionAmount:"10 pessoas",  
-        descriptionMoreInformation:"Isso é o que chamamos de empreendedorismo de lifestyle. Somos pioneiros na disseminação deste conceito e no apoio a estes negócios em nosso país, contribuindo para torná-los mais liberado, mais regulado, mais conhecido e reafirmá-lo como tendência de negócio emergente na sociedade brasileira. Diante disso, preparamos um workshop para orientar e capacitar empreendedores que queiram fazer ou tenham negócios nesse mercado de lifestyle."
-    },
-
-    event5Object:{
-        title:"Tapa na Cara",
-        local:"55Lab.Work",
-        image:"/assets/images/image-curse-tapa-cara.png",
-        description:"Um choque de realidade para você entender que empreender com planejamento é possível e muito menos arriscado!",
-        descriptionOrganizer:"Acelere.me",
-        descriptionSupporter:"55Lab.Work",
-        descriptionDate:"04 e 06 de abril",
-        descriptionAmount:"15 pessoas",  
-        descriptionMoreInformation:"O Tapa na Cara é um evento que tem 16 horas seguidas de capacitação por meio de experiência empreendedora com a metodologia de implementação de negócios do Acelere.me. Este evento busca dar um “tapa na cara” dos participantes para que eles possam entender que, apesar de não ser fácil, empreender com planejamento, metas e objetivos claros é possível e muito menos arriscado."
-    },
-
-    event6Object:{
-        title:"Experience",
-        local:"MODIFICAR",
-        image:"/assets/images/background-image.png",
-        description:"MODIFICAR é o evento de conteúdo e experiência do Acelere.me que estimula a percepção que empreender é muito mais do que ter uma empresa ou um CNPJ.",
-        descriptionOrganizer:"MODIFICAR",
-        descriptionSupporter:"MODIFICAR",
-        descriptionDate:"MODIFICAR",
-        descriptionAmount:"MODIFICAR",  
-        descriptionMoreInformation:"No Experience você pode participar de um dia de trabalho para entrar em contato com  a metodologia de implementação de negócios do Acelere.me, descobrir a importância de definição de propósito para o seu projeto e aprender sobre desenvolvimento de modelo de negócios, metas e plano de ação."
-    },
-
-    event7Object:{
-        title:"Maratona Ignition",
-        local:"CTJ",
-        image:"/assets/images/background-image.png",
-        description:"Evento do Acelere.me que busca pré-acelerar negócios e empresas por meio de intensas capacitações e mentorias com profissionais do mercado.",
-        descriptionOrganizer:"Acelere.me",
-        descriptionSupporter:"55Lab",
-        descriptionDate:"14,15 e 16 de julho",
-        descriptionAmount:"50 pessoas",  
-        descriptionMoreInformation:"O Maratona Ignition é conhecido como o primeiro programa de pré-aceleração de negócios e empresas desenvolvido no Distrito Federal. Na última edição, 10 ideias e projetos de negócios foram selecionados para participar de 48h intensas de capacitações com experiências empreendedoras e mentorias com profissionais do mercado."
     }
+
 });
