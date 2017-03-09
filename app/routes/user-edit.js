5dcec54a9c4ac28d62bf659fdcfbf0451ef3c0ec
thiagotmb <tmb0710@gmail.com>
2017-03-09 01:53:00 -0500

    Adding person and user-edit relation

--- app/routes/user-edit.js
+++ app/routes/user-edit.js
@@ -2,7 +2,14 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin,{
-    model() {
-        return this.store.findAll('address');
-    }
+  model() {
+
+    var addressRecord = this.store.query('person', {
+      include: 'addressPoint'
+    }).then(function(item) {
+      alert(item.get('addressPoint'))
+    });
+    // ...after the record has loaded
+  return addressRecord;
+}
 });
